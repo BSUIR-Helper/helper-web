d45e2f2215abe0e642e6a357084bee5f3de7e83b
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2014-02-02 01:02:36 +0300

    Remove extra authorize
    It's involved from `set_discipline` which is called as `before_action`.

--- app/controllers/disciplines_controller.rb
+++ app/controllers/disciplines_controller.rb
@@ -42,7 +42,6 @@ class DisciplinesController < ApplicationController
   # PATCH/PUT /disciplines/1
   # PATCH/PUT /disciplines/1.json
   def update
-    authorize @discipline
     respond_to do |format|
       if @discipline.update(discipline_params)
         format.html { redirect_to @discipline, notice: 'Discipline was successfully updated.' }
