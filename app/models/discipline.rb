69258b8958c78c708d40287a875043d362f82e50
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2014-02-01 12:06:28 +0300

    Discipline must have name or abbr

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,2 +1,7 @@
 class Discipline < ActiveRecord::Base
+  validate :name_or_abbr_exist
+
+  def name_or_abbr_exist
+    errors.add :base, 'Name or abbreviation must be set' if name.blank? and abbr.blank?
+  end
 end
--- test/models/discipline_test.rb
+++ test/models/discipline_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class DisciplineTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "doesn't save without name and abbr" do
+    discipline = Discipline.new
+    assert !discipline.save, "Saved the discipline without a name"
+  end
+
+  test "saves with name only" do
+    discipline = Discipline.new
+    discipline.name = "Mathematics"
+    assert discipline.save, "Didn't save the discipline without an abbr"
+  end
+
+  test "saves with abbr only" do
+    discipline = Discipline.new
+    discipline.abbr = "OAIP"
+    assert discipline.save, "Didn't save the discipline without a name"
+  end
 end
