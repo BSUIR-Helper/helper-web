01d817111b4a09330f1499439f00adeeb6481b05
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2014-02-15 17:43:12 +0300

    Test User#to_s

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,19 @@ describe User do
     user = User.new
     expect(user.admin?).to be_false
   end
+
+  describe 'to_s' do
+    it 'returns email when name is not set' do
+      subject.email = 'asd@asd.asd'
+
+      expect(subject.to_s).to eq(subject.email)
+    end
+
+    it 'returns name when email and name are set' do
+      subject.email = 'asd@asd.asd'
+      subject.name = 'Asd'
+
+      expect(subject.to_s).to eq('Asd')
+    end
+  end
 end
